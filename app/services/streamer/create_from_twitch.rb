4cc144587044ac658e6074554fc060ffcd5fffa8
swol1 <den.gavrilin1@gmail.com>
2025-05-31 13:29:37 +0200

    When twitch user changes login, it is possible to subscribe to new login

--- app/services/streamer/create_from_twitch.rb
+++ app/services/streamer/create_from_twitch.rb
@@ -11,15 +11,19 @@ class Streamer::CreateFromTwitch < BaseService
   def call
     raise NotFoundError unless (streamer_data = fetch_streamer_data.presence)
 
-    Streamer.create!(
-      login: streamer_data[:login],
-      twitch_id: streamer_data[:id],
-      name: streamer_data[:display_name]
-    ).tap do |streamer|
+    # Twitch user can change its login and twitch_id will be the same but login is not
+    # maybe it's better to unsubscribe users->destroy/create instead in this case, will see
+    streamer = Streamer.find_or_initialize_by(twitch_id: streamer_data[:id])
+    is_new = streamer.new_record?
+    streamer.login = streamer_data[:login]
+    streamer.name = streamer_data[:display_name]
+    streamer.save!
+    if is_new
       Streamer::UpdateInfo.call(streamer)
       Streamer::SubscribeToTwitchEventsJob.perform_async(streamer.id)
       Streamer::ReconcileEnabledTwitchEventsJob.perform_in(10.minutes, streamer.id)
     end
+    streamer
   end
 
   private
